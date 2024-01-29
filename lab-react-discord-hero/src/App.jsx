8eb84ce4cc2405891b2b110160457fad6d663ae6
Emmanouil Popas <noah.popas@hotmail.com>
2024-01-29 18:08:14 +0100

    finished lab

---
+++ lab-react-discord-hero/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './Header';
+import HomePage from './components/HomePage';
+
+function App() {
+  return (
+    <div>
+
+    <Header/>
+    <HomePage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ lab-react-discord-hero/src/Header.jsx
@@ -0,0 +1,18 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">
+                <img src="https://camo.githubusercontent.com/b10e2af5f0bd48fc41b772cb03ac3b1f9b2326770dee069b9fa4fdc0af97de84/68747470733a2f2f656475636174696f6e2d7465616d2d323032302e73332e65752d776573742d312e616d617a6f6e6177732e636f6d2f7765622d6465762f6c6162732f6c616e64696e672d706167652f646973636f72642d6c6f676f2d62672e706e67"></img>
+            </div>
+            <nav>
+                <ul>
+                    <li>
+                        <img className="menu" src="https://camo.githubusercontent.com/2be59d01021f32bfde92a29c4f5db89cf5325731e63fad34d6bb50ba64943c16/68747470733a2f2f656475636174696f6e2d7465616d2d323032302e73332e65752d776573742d312e616d617a6f6e6177732e636f6d2f7765622d6465762f6c6162732f6c616e64696e672d706167652f6d656e752d69636f6e2d62672e706e67"></img>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+export default Header;
