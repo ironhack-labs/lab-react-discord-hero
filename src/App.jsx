c559adbb0b773f4ee5a45d7bf93decb72ae78188
Maksym Synytskyi <citomax7196@gmail.com>
2024-01-29 12:50:03 +0100

    small fix

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,16 @@ import menu from './assets/menu-icon.png'
 function App() {
   return (
     <div className="app">
-      <div className="container">
-        <div className="header">
-          <img src={logo} className="logo" alt="logo" width='160' />
-          <img src={menu} className="menu" alt="logo" width='40' />
-        </div>
-        <div className="main-content">
-          <h1>IMAGINE A PLACE...</h1>
-          <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
-          <button className="btn">Download for Mac</button>
-          <button className="btn btn-black">Open Discord in your browser</button>
-        </div>
+      <div className="header">
+        <img src={logo} className="logo" alt="logo" width='160' />
+        <img src={menu} className="menu" alt="logo" width='40' />
       </div>
-
+      <div className="main-content">
+        <h1>IMAGINE A PLACE...</h1>
+        <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
+        <button className="btn">Download for Mac</button>
+        <button className="btn btn-black">Open Discord in your browser</button>
+      </div>
     </div>
   );
 }
