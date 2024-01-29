9a12521c4c289a3a63e2d76142468d35685105a1
Emmanouil Popas <noah.popas@hotmail.com>
2024-01-29 18:09:17 +0100

    solved lab

--- lab-react-discord-hero/src/components/HomePage.jsx
+++ lab-react-discord-hero/src/components/HomePage.jsx
@@ -1,16 +1,23 @@
 const HomePage = () => {
-    return (
-      <div className="page">
-        <main>
-          <p className="title">Imagine a place...</p>
-          <p className="paragraph">... where you can belong to a school-club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. </p>
-          <img className="img-page" src="https://camo.githubusercontent.com/a7e491bd71660eb9af8f34bee7295561148a4d7dd3828053aea708cd0e260578/68747470733a2f2f656475636174696f6e2d7465616d2d323032302e73332e65752d776573742d312e616d617a6f6e6177732e636f6d2f7765622d6465762f6c6162732f6c616e64696e672d706167652f646973636f72642d6261636b67726f756e642e706e67" alt="Placeholder" />
-        </main>
-        <button className="btn1">Donwload for Mac</button>
-        <button className="btn2">Open Discord on Browser</button>
+  return (
+    <div className="page">
+      <main>
+        <p className="title">Imagine a place...</p>
+        <p className="paragraph">
+          ... where you can belong to a school-club, a gaming group, or a
+          worldwide art community. Where just you and a handful of friends can
+          spend time together.{" "}
+        </p>
+        <img
+          className="img-page"
+          src="https://camo.githubusercontent.com/a7e491bd71660eb9af8f34bee7295561148a4d7dd3828053aea708cd0e260578/68747470733a2f2f656475636174696f6e2d7465616d2d323032302e73332e65752d776573742d312e616d617a6f6e6177732e636f6d2f7765622d6465762f6c6162732f6c616e64696e672d706167652f646973636f72642d6261636b67726f756e642e706e67"
+          alt="Placeholder"
+        />
+      </main>
+      <button className="btn1">Donwload for Mac</button>
+      <button className="btn2">Open Discord on Browser</button>
+    </div>
+  );
+};
 
-      </div>
-    );
-  }
-  
-  export default HomePage;
+export default HomePage;
