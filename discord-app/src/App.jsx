76195cc72f4c348c7819096efe7e24ee469133a6
samanta-scavassa <samanta.camargoterra@gmail.com>
2024-01-29 17:24:20 +0100

    discord app

---
+++ discord-app/src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <img src="../src/assets/discord-logo-white.png" />
+        <img src="../src/assets/menu-icon.png" />
+      </nav>
+      <div className="info">
+        <h1>IMAGINE A PLACE...</h1>
+        <p>
+          ...Where you can belong to a school club, a gaming group or a<br />{" "}
+          worldwide arte community. Where just you and a handful of
+          <br /> friends can spend time together. A place that makes it easy to
+          <br /> talk every day and hang out more often.
+        </p>
+        <div className="download-section">
+          <div className="info-buttons">
+            <button id="download-btn">Download for Mac</button>
+            <button id="open-btn">Open Discord in your browser</button>
+          </div>
+          <img src="../src/assets/discord-background.png" />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
