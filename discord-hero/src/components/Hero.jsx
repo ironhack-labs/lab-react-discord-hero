5f6270c1838fb38ffdb9d774b6714df4d71f1199
Tesa Musa <kujtesamusa1@gmail.com>
2024-02-05 17:33:36 +0100

    Complete four iterations, work in progress

---
+++ discord-hero/src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+function Hero(){
+
+    return (
+
+        
+    <div>
+          <br />,
+    <h1> PLACE...</h1>,
+   
+    
+    <p>... where you can belong to a school club, a gaming group, or a worldwide art community
+        .Where just you and a handful of freinds can spend time together.A place that makes
+        it easy to talk every day and hang out more often
+    </p><h1>IMAGINE A</h1>,
+        </div>
+  
+    
+    )
+    
+   
+
+}
+
+export default Hero;
