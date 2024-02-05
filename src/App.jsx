de81e0e21db70985a3a3e7640fafc94256eb5edb
Kumar Daryanani <destral@msn.com>
2024-02-05 14:57:34 +0100

    iteration 1, WIP

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// src/App.jsx
+import './App.css'
+import Header from './components/Header.jsx';
+import Section from './components/Section.jsx';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <Section/>
+    </div>
+  );
+}
+
+export default App
