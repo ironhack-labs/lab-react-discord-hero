e2c2bd4763494d5cb1ae5ea6a9778b0cd596085b
Oscar Gomez Diez <oscarfotodiez@gmail.com>
2024-02-06 00:49:48 +0100

    lab-casi-done

---
+++ discord/src/components/Photo/Photo.jsx
@@ -0,0 +1,9 @@
+import Photo from './../../assets/discord-background.png'
+import './../Photo/Photo.css'
+
+const Image = () => {
+    return (
+        <img src={Photo} alt="Background image" />
+    )
+}
+export default Image
---
+++ discord/src/components/Title/Title.jsx
@@ -0,0 +1,6 @@
+
+import './Title.css'
+const Title1 = () => {
+    return <h1>Imagine a <br></br>place ...</h1>
+}
+export default Title1
