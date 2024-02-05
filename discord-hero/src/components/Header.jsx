da3ff40677f8b7f958fc396ab21c83192d2588e7
Senay Asikoglu <senayasikoglu@gmail.com>
2024-02-05 16:42:59 +0100

    first four iterations were solved

---
+++ discord-hero/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import logo from "../assets/discord-logo-white.png"
+import menu from "../assets/menu-icon.png"
+function Header(){
+
+   
+
+    return (
+        <>
+              <img src={logo} alt="example" /> 
+              <img src={menu} alt="example" /> 
+
+
+        </>
+    )
+        
+}
+
+export default Header;
