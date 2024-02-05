de2691643e971859430fac64c7129af6bd598769
MelSchiffner <156306012+MelSchiffner@users.noreply.github.com>
2024-02-05 21:21:30 +0100

    Lab done

---
+++ react-discord-hero/src/components/Content.jsx
@@ -0,0 +1,19 @@
+function Content (){
+    return (
+        <div>
+            <h1>IMAGINE A PLACE...</h1>
+            <p>
+                ...where you can belong to a school club, a gaming group, or a 
+                worldwide art community. Where just you and a handful of 
+                friends can spend time together. A place that makes it easy to 
+                talk every day and hang out more often.
+            </p>
+            <button>Download for Mac</button>
+            <a>Open Discord in your browser</a>
+
+
+        </div>
+    )
+}
+
+export default Content;
