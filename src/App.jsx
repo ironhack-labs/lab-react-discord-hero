e741d5caa10d41963506965f064dc085b891d156
Barbara Lan <barbie.lancuba@gmail.com>
2024-02-05 14:58:09 +0100

    first lab

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Text from './Text'
+import Images from './Images'
+
+
+
+function App() {
+
+  return (
+     <>
+     <Text/>
+     <Images/>
+     </>
+  )
+}
+
+export default App
