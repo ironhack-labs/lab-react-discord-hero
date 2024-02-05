bfa34e33f5f727c7568da14d61df29a7bb5ccc4d
Alina Havriushenko <alinagvr@gmail.com>
2024-02-05 20:22:35 +0100

    lab done

---
+++ Discord Hero/src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <nav>
+      <img src="./src/assets/discord-logo-white.png" alt="logo" />
+      <img className="menu-img" src="./src/assets/menu-icon.png" />
+    </nav>
+  );
+}
