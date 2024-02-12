f9826b9b7fcc0b93e0628bb4ff94f00146f87e6c
pablosanchezcamara89@gmail.com <pablosanchez@MacBook-Air-de-Pablo.local>
2024-02-12 18:08:20 +0100

    Progreso

--- react/src/components/Body.jsx
+++ react/src/components/Body.jsx
@@ -1,15 +1,34 @@
 import pnjDiscord from "../assets/personajes-discord.png";
 
 function Body() {
-  const divStyle = {}
+  const divStyle = {
+    display: "flex",
+    alignItems: "center",
+    height: "400px"
+  }
+
+  const contentStyle = {
+    display: "flex",
+    flexDirection: "column",
+    
+  }
+
+  const imgStyle ={
+    alignItems: "end",
+    justifyContent: "end",
+    margin: "0px",
+    padding: "0px",
+  }
 
   return (
     <div style={divStyle}>
+      <div style={contentStyle}> 
       <h1>IMAGINE A PLACE...</h1>
       <p>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
       <button>Donwload for Mac</button>
-      <button>Oopen Discord in your browser</button>
-      <img src={pnjDiscord} width="400px" alt="menu-icon" />
+      <button>Open Discord in your browser</button>
+      </div>
+      <img src={pnjDiscord} width="400px" alt="menu-icon" style={imgStyle}/>
     </div>
   );
 }
