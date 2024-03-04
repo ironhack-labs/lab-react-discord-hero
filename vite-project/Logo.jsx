1259cabcb4013c885e43dfefa2c3ee326226ba5d
Michael R Misruell <m.misurell@gmail.com>
2024-03-04 16:22:38 +0100

    done

---
+++ vite-project/Logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import logoImage from "./src/assets/discord-logo-white.png"; // Update the path based on your project structure
+import "./src/index.css";
+
+const Logo = () => {
+  return <img src={logoImage} alt="Discord Logo" />;
+};
+
+export default Logo;
---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+// App.jsx
+
+import React from "react";
+import Logo from "/Logo"; // Update the path based on your project structure
+
+function App() {
+  return (
+    <div>
+      <h1>Discord</h1>
+      <Logo />
+      {/* Other components or content */}
+    </div>
+  );
+}
+
+export default App;
