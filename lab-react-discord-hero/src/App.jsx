186271aeab2abd072791cd3de98e908c918968b5
Hemyulin <daniel.budiansky@protonmail.com>
2024-03-04 12:10:58 +0100

    A lot is done apart from the buttons

--- lab-react-discord-hero/src/App.jsx
+++ lab-react-discord-hero/src/App.jsx
@@ -1,8 +1,27 @@
 // src/App.jsx
 import "./App.css";
+import { Button } from "./components/Button";
+import { Navbar } from "./components/Navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <body>
+        <Navbar />
+        <div className="text-block">
+          <h1>IMAGINE A PLACE...</h1>
+          <p>
+            Where you can belong to a school club, a gaming group, or a
+            worldwide art community. Where just you and a handful of friends can
+            spend time together. A place that makes it easy to talk every day
+            and hang out more often.
+          </p>
+        </div>
+        <Button />
+        <Button />
+      </body>
+    </div>
+  );
 }
 
 export default App;
---
+++ lab-react-discord-hero/src/components/Button.jsx
@@ -0,0 +1,5 @@
+import "./Button.css";
+
+export const Button = () => {
+  return <div className="button">Download for Mac</div>;
+};
