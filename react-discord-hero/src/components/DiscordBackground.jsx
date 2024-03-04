8e2e39e901a308f0ec4ae7d59cfa18d20434a059
PaulinaWW <159482754+PaulinaWW@users.noreply.github.com>
2024-03-04 15:04:19 +0100

    Add files via upload

---
+++ react-discord-hero/src/components/DiscordBackground.jsx
@@ -0,0 +1,12 @@
+import discordBackground from '../assets/discord-background.png';
+
+function DiscordBackground(){
+    return (
+        <div className='Background'>
+            <img src={discordBackground} alt ="Discord Background"/>
+        </div>
+    );
+}
+
+
+export default DiscordBackground; 
