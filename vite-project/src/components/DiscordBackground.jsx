880132e6c68d1abd65671c8a69e117f2791bc330
Nwaigba66 <glorianwaigba@gmail.com>
2024-03-05 13:16:44 +0100

    done

---
+++ vite-project/src/components/DiscordBackground.jsx
@@ -0,0 +1,12 @@
+import  image from '../assets/discord-background.png'
+
+const DiscordBackground = () => {
+  return (
+    <div className='Background'>
+      <img src={image} alt='DiscordBackground'/>
+
+    </div>
+  )
+}
+
+export default DiscordBackground
---
+++ vite-project/src/components/MenuLogo.jsx
@@ -0,0 +1,11 @@
+import menuIcon from "../assets/menu-icon.png";
+
+const MenuLogo = () => {
+  return (
+    <div className="logo">
+        <img src={menuIcon} alt="Menu Logo" />
+    </div>
+  )
+}
+
+export default MenuLogo
